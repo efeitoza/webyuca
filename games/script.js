2fa0cf91811beafc14945f40f81c529b48653d47
efeitoza <labsyuca@gmail.com>
2025-07-29 19:54:54 -0600

    Add files via upload

--- games/script.js
+++ games/script.js
@@ -1,49 +1,33 @@
-const imagens = [
-  '🍎', '🐶', '🚗', '🌟'
-];
-
-let cartas = [];
-let cartasViradas = [];
-let bloqueado = false;
+const imagens = ['🍎', '🐶', '🚗', '🌟'];
+let cartas = [], cartasViradas = [], bloqueado = false;
 
 function embaralhar(array) {
   return array.concat(array)
     .sort(() => 0.5 - Math.random())
-    .map((emoji, index) => ({
-      id: index,
-      emoji,
-      revelada: false
-    }));
+    .map((emoji, index) => ({ id: index, emoji, revelada: false }));
 }
 
 function criarTabuleiro() {
   const tabuleiro = document.getElementById('tabuleiro');
   tabuleiro.innerHTML = '';
   cartas = embaralhar(imagens);
-
   cartas.forEach((carta, index) => {
     const div = document.createElement('div');
     div.classList.add('carta');
     div.dataset.index = index;
-
     const span = document.createElement('span');
     span.textContent = carta.emoji;
-    span.style.fontSize = '2em';
     div.appendChild(span);
-
     div.addEventListener('click', () => virarCarta(index, div));
-
     tabuleiro.appendChild(div);
   });
 }
 
 function virarCarta(index, el) {
   if (bloqueado || cartas[index].revelada || cartasViradas.length === 2) return;
-
   cartas[index].revelada = true;
   el.classList.add('revelada');
   cartasViradas.push({ index, el });
-
   if (cartasViradas.length === 2) {
     const [c1, c2] = cartasViradas;
     if (cartas[c1.index].emoji === cartas[c2.index].emoji) {
@@ -66,5 +50,4 @@ function reiniciarJogo() {
   cartasViradas = [];
   criarTabuleiro();
 }
-
 criarTabuleiro();
